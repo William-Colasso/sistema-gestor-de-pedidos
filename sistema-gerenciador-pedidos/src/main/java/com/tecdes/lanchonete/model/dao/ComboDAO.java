7b30d5a3f328603ea92090f707b3b5148f944b88
ArthurJoseBona <arthurjosebona@gmail.com>
2025-11-21 17:05:40 -0300

    add: adicionar FuncionarioDAO e ComboDAO

---
+++ sistema-gerenciador-pedidos/src/main/java/com/tecdes/lanchonete/model/dao/ComboDAO.java
@@ -0,0 +1,274 @@
+package com.tecdes.lanchonete.model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.tecdes.lanchonete.config.ConnectionFactory;
+import com.tecdes.lanchonete.model.entity.CategoriaProduto;
+import com.tecdes.lanchonete.model.entity.Combo;
+import com.tecdes.lanchonete.model.entity.Item;
+import com.tecdes.lanchonete.model.entity.Produto;
+import com.tecdes.lanchonete.model.enums.TipoItem;
+
+public class ComboDAO implements InterfaceDAO<Combo> {
+
+    @Override
+    public void create(Combo t) {
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            insertItem(conn, t);
+            insertCombo(conn, t);
+            insertProdutos(conn, t);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao criar Combo: " + e);
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            removerProdutosCombo(conn, id);
+            removerCombo(conn, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao deletar Combo: " + e);
+        }
+    }
+
+    @Override
+    public void update(Combo t) {
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            updateItem(t, conn);
+            updateCombo(t, conn);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao atualizar Combo: " + e);
+        }
+    }
+
+    @Override
+    public Combo getById(Long id) {
+        String sql = """
+            SELECT i.*, c.vl_desconto 
+            FROM t_sgp_combo c
+            INNER JOIN t_sgp_item i ON i.id_item = c.ITEM_id_item
+            WHERE c.ITEM_id_item = ?
+        """;
+        try (Connection conn = ConnectionFactory.getConnection();
+        PreparedStatement pr = conn.prepareStatement(sql)) {
+            pr.setLong(1, id);
+            ResultSet rs = pr.executeQuery();
+            if (rs.next()) {
+                Combo combo = mapCombo(rs, conn);
+                return combo;
+            }
+
+            return null;
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao obter Combo");
+        }
+    }
+
+    @Override
+    public List<Combo> getAll() {
+        return null;
+    }
+
+    private void fillInsertItemParameters(PreparedStatement pr, Item item) throws SQLException{
+        pr.setString(1, item.getNome());
+        pr.setString(2, item.getDescricao());
+        pr.setString(3, String.valueOf(item.getTipoItem().getValue()));
+        pr.setDate(4, item.getDataCriacao());
+        pr.setInt(5, item.getStatusAtivo());
+    }
+
+    private void fillUpdateItemParameters(PreparedStatement pr, Item item) throws SQLException{
+        fillInsertItemParameters(pr, item);
+        pr.setLong(6, item.getId());
+    }
+
+    private void insertItem(Connection conn, Item item) {
+        String sqlItem = """
+            INSERT INTO t_sgp_item (
+                nm_item, ds_item, tp_item, dt_criacao, st_ativo
+            ) VALUES (?, ?, ?, ?, ?)
+        """;
+
+        try (PreparedStatement pr = conn.prepareStatement(sqlItem, Statement.RETURN_GENERATED_KEYS)) {
+            fillInsertItemParameters(pr, item);
+            pr.executeUpdate();
+            ResultSet rs = pr.getGeneratedKeys();
+            if (rs.next()) {
+                item.setId(rs.getLong(1));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao inserir item Combo no banco: " + e);
+        }
+    }
+
+    private void insertCombo(Connection conn, Combo combo) {
+        String sqlCombo = """
+            INSERT INTO t_sgp_combo (
+                ITEM_id_item, vl_desconto
+            ) VALUES (?, ?)   
+        """;
+
+        try (PreparedStatement pr = conn.prepareStatement(sqlCombo)) {
+            pr.setLong(1, combo.getId());
+            pr.setInt(2, combo.getDesconto());
+
+            pr.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao inserir combo no banco: " + e);
+        }
+    }
+
+    private void insertProdutos(Connection conn, Combo combo) {
+        String sql = """
+            INSERT INTO t_produto_combo (
+                PRODUTO_id_item, COMBO_ITEM_id_item
+            ) VALUES (?, ?)
+        """;
+
+        try (PreparedStatement pr = conn.prepareStatement(sql)) {
+            for (Produto p : combo.getProdutos()) {
+                pr.setLong(1, p.getId());
+                pr.setLong(2, combo.getId());
+
+                pr.executeUpdate();
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    private void removerProdutosCombo(Connection conn, Long id) {
+        String sql = "DELETE FROM t_produto_combo WHERE COMBO_ITEM_id_item = ?";
+
+        try (PreparedStatement pr = conn.prepareStatement(sql)) {
+            pr.setLong(1, id);
+
+            pr.executeUpdate();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    private void removerCombo(Connection conn, Long id) {
+        String sql = "DELETE FROM t_sgp_combo WHERE ITEM_id_item = ?";
+
+        try (PreparedStatement pr = conn.prepareStatement(sql)) {
+            pr.setLong(1, id);
+
+            pr.executeUpdate();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    private void updateItem(Item item, Connection conn) {
+        String sql = """
+            UPDATE t_sgp_item
+            SET nm_item = ?, ds_item = ?, tp_item = ?, dt_criacao = ?, st_ativo = ?
+            WHERE id_item = ?
+        """;
+
+        try (PreparedStatement pr = conn.prepareStatement(sql)) {
+            fillUpdateItemParameters(pr, item);
+            pr.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao atualizar item combo: " + e);
+        }
+    }
+
+    private void updateCombo(Combo combo, Connection conn) {
+        String sql = """
+            UPDATE t_sgp_combo
+            SET vl_desconto = ?
+            WHERE ITEM_id_item = ?        
+        """;
+
+        try (PreparedStatement pr = conn.prepareStatement(sql)) {
+            pr.setInt(1, combo.getDesconto());
+            pr.setLong(2, combo.getId());
+            pr.executeUpdate();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    private Combo mapCombo(ResultSet rs, Connection conn) throws SQLException {
+        Combo combo = new Combo();
+
+        mapItemBase(rs, combo);
+        combo.setTipoItem(TipoItem.COMBO);
+        combo.setDesconto(rs.getInt("vl_desconto"));
+        
+        List<Produto> produtos = getProdutosFromCombo(rs.getLong("id_item"), conn);
+        combo.setProdutos(produtos);
+
+        return combo;
+    }
+
+    private void mapItemBase(ResultSet rs, Item item) throws SQLException {
+        item.setId(rs.getLong("id_item"));
+        item.setNome(rs.getString("nm_item"));
+        item.setDescricao(rs.getString("ds_item"));
+        item.setDataCriacao(rs.getDate("dt_criacao"));
+        item.setStatusAtivo(rs.getInt("st_ativo"));
+    }
+
+    private List<Produto> getProdutosFromCombo (Long id, Connection conn) {
+
+        String sql = """
+            SELECT 
+                i.*, p.vl_produto, p.CATEGORIA_PRODUTO_id_categoria, ct.nm_categoria, ct.sg_categoria   
+                FROM t_produto_combo pc INNER JOIN t_sgp_produto p ON pc.PRODUTO_id_item = p.ITEM_id_item   
+                INNER JOIN t_sgp_item i ON p.ITEM_id_item = i.id_item 
+                INNER JOIN t_sgp_categoria_produto ct ON p.CATEGORIA_PRODUTO_id_categoria = ct.id_categoria 
+            WHERE pc.COMBO_ITEM_id_item = ?
+        """;
+
+        List<Produto> produtos = new ArrayList<>();
+
+        try (PreparedStatement pr = conn.prepareStatement(sql)) {
+
+            pr.setLong(1, id);
+
+            ResultSet rs = pr.executeQuery();
+
+            while (rs.next()) {
+                produtos.add(mapProduto(rs));
+            }
+            
+        } catch (Exception e) {
+            throw new RuntimeException("Erro DAO: Falha ao obter Produtos do Combo: " + e);
+        }
+
+        return produtos;
+    }
+
+    private Produto mapProduto(ResultSet rs) throws SQLException {
+        Produto produto = new Produto();
+
+        mapItemBase(rs, produto);
+        produto.setTipoItem(TipoItem.PRODUTO);
+        produto.setValor(rs.getDouble("vl_produto"));
+        produto.setCategoria(mapCategoriaProduto(rs));
+
+        return produto;
+    }
+
+    private CategoriaProduto mapCategoriaProduto(ResultSet rs) throws SQLException{
+        CategoriaProduto categoria = new CategoriaProduto();
+
+        categoria.setId(rs.getLong("CATEGORIA_PRODUTO_id_categoria"));
+        categoria.setNome(rs.getString("nm_categoria"));
+        categoria.setSigla(rs.getString("sg_categoria"));
+
+        return categoria;
+    }
+
+}
--- sistema-gerenciador-pedidos/src/main/java/com/tecdes/lanchonete/model/dao/FuncionarioDAO.java
+++ sistema-gerenciador-pedidos/src/main/java/com/tecdes/lanchonete/model/dao/FuncionarioDAO.java
@@ -1,41 +1,230 @@
 package com.tecdes.lanchonete.model.dao;
 
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.tecdes.lanchonete.config.ConnectionFactory;
 import com.tecdes.lanchonete.model.entity.Funcionario;
+import com.tecdes.lanchonete.model.entity.Gerente;
 
 public class FuncionarioDAO implements InterfaceDAO<Funcionario> {
 
     @Override
     public void create(Funcionario t) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'create'");
+        try(Connection conn = ConnectionFactory.getConnection()) {
+            t.setId(insertFuncionario(t, conn));
+            if (t instanceof Gerente) {
+                insertGerente((Gerente) t, conn);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro DAO ao criar funcionário: " + e);
+        }
     }
-
+    
     @Override
     public void delete(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            // Primeiro, deletar da tabela de gerentes caso exista
+            String deleteGerenteSql = "DELETE FROM t_sgp_gerente WHERE FUNCIONARIO_id_funcionario = ?";
+            try (PreparedStatement pr = conn.prepareStatement(deleteGerenteSql)) {
+                pr.setLong(1, id);
+                pr.executeUpdate();
+            }
+
+            // Deletar da tabela de funcionários
+            String deleteFuncionarioSql = "DELETE FROM t_sgp_funcionario WHERE id_funcionario = ?";
+            try (PreparedStatement pr = conn.prepareStatement(deleteFuncionarioSql)) {
+                pr.setLong(1, id);
+                pr.executeUpdate();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao deletar Funcionario: " + e);
+        }
     }
 
     @Override
     public void update(Funcionario t) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            updateFuncionario(t, conn);
+            if (t instanceof Gerente) {
+                updateGerente((Gerente) t, conn);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao atualizar Funcionario: " + e);
+        }
     }
 
     @Override
     public Funcionario getById(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getById'");
+        String sql = """
+            SELECT f.*, 
+                gf.ds_senha AS chefe_senha, gf.nm_login AS chefe_login,   -- Gerente do funcionário
+                g.ds_senha AS gerente_senha, g.nm_login AS gerente_login  -- Se o funcionário for gerente
+            FROM t_sgp_funcionario f 
+            LEFT JOIN t_sgp_gerente gf ON gf.FUNCIONARIO_id_funcionario = f.FUNCIONARIO_id_gerente
+            LEFT JOIN t_sgp_gerente g ON g.FUNCIONARIO_id_funcionario = f.id_funcionario
+            WHERE id_funcionario = ? 
+        """;
+        try(Connection conn = ConnectionFactory.getConnection();
+        PreparedStatement pr = conn.prepareStatement(sql)) {
+            pr.setLong(1, id);
+            ResultSet rs = pr.executeQuery();
+            
+            if (rs.next()) {
+                return mapFuncionario(rs);
+            }
+
+            return null;
+        } catch (Exception e) {
+            throw new RuntimeException("Erro DAO ao obter Funcionario por id: " + e);
+        }
     }
 
     @Override
     public List<Funcionario> getAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+        String sql = """
+            SELECT f.*, 
+                gf.ds_senha AS chefe_senha, gf.nm_login AS chefe_login,   -- Gerente do funcionário
+                g.ds_senha AS gerente_senha, g.nm_login AS gerente_login -- Se o funcionário for gerente
+            FROM t_sgp_funcionario f 
+            LEFT JOIN t_sgp_gerente gf ON gf.FUNCIONARIO_id_funcionario = f.FUNCIONARIO_id_gerente
+            LEFT JOIN t_sgp_gerente g ON g.FUNCIONARIO_id_funcionario = f.id_funcionario
+        """;
+
+        try(Connection conn = ConnectionFactory.getConnection();
+        PreparedStatement pr = conn.prepareStatement(sql)) {
+            ResultSet rs = pr.executeQuery();
+
+            List<Funcionario> listFuncionarios = new ArrayList<>();
+
+            while (rs.next()) {
+                listFuncionarios.add(mapFuncionario(rs));
+            }
+
+            return listFuncionarios;
+        } catch (Exception e) {
+            throw new RuntimeException("Erro DAO ao buscar todos os funcionários: " + e);
+        }
     }
 
- 
-    
+    private void fillInsertStatementParameters(PreparedStatement pr, Funcionario funcionario) throws SQLException{
+        if (funcionario.getGerente() != null) {
+            pr.setLong(1, funcionario.getGerente().getId());
+        } else {
+            pr.setNull(1, Types.INTEGER);
+
+        }
+        pr.setString(2, funcionario.getNome());
+        pr.setDate(3, funcionario.getDataNascimento());
+        pr.setString(4, funcionario.getCpf());
+    }
+
+    private Long insertFuncionario(Funcionario funcionario, Connection conn) {
+        String sql = """
+            INSERT INTO t_sgp_funcionario (
+                FUNCIONARIO_id_gerente, nm_funcionario, dt_nascimento, nr_cpf
+            ) VALUES (?, ?, ?, ?)
+        """;
+
+        try(PreparedStatement pr = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            fillInsertStatementParameters(pr, funcionario);
+            pr.executeUpdate();
+            ResultSet rs = pr.getGeneratedKeys();
+            if (rs.next()) {
+                return rs.getLong(1);
+            }
+
+            return null;
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao inserir funcionário: " + e);
+        }
+    }
+
+    private void insertGerente(Gerente gerente, Connection conn) {
+        String sql = """
+            INSERT INTO t_sgp_gerente (
+                FUNCIONARIO_id_funcionario, ds_senha, nm_login
+            ) VALUES (?, ?, ?)     
+        """;
+
+        try (PreparedStatement pr = conn.prepareStatement(sql)) {
+            pr.setLong(1, gerente.getId());
+            pr.setString(2, gerente.getSenha());
+            pr.setString(3, gerente.getLogin());
+
+            pr.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao inserir Gerente: " + e);
+        }
+    }
+
+    private void updateFuncionario(Funcionario funcionario, Connection conn) {
+        String sql = """
+            UPDATE t_sgp_funcionario 
+            SET FUNCIONARIO_id_gerente = ?, nm_funcionario = ?, dt_nascimento = ?, nr_cpf = ? 
+            WHERE id_funcionario = ?
+        """;
+        try(PreparedStatement pr = conn.prepareStatement(sql)) {
+            fillUpdateStatementParameters(pr, funcionario);
+            pr.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao atualizar Funcionario: " + e);
+        }
+    }
+
+    private void updateGerente(Gerente gerente, Connection conn) {
+        String sql = """
+            UPDATE t_sgp_gerente   
+            SET nm_login = ?, ds_senha = ?   
+            WHERE FUNCIONARIO_id_funcionario = ?
+        """;
+        try (PreparedStatement pr = conn.prepareStatement(sql)) {
+            pr.setString(1, gerente.getLogin());
+            pr.setString(2, gerente.getSenha());
+            pr.setLong(3, gerente.getId());
+
+            pr.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao atualizar Gerente: " + e);
+        }
+    }
+
+    private void fillUpdateStatementParameters(PreparedStatement pr, Funcionario funcionario) throws SQLException {
+        fillInsertStatementParameters(pr, funcionario);
+        pr.setLong(5, funcionario.getId());
+    }
+
+    private Funcionario mapFuncionario(ResultSet rs) throws SQLException{
+        Funcionario funcionario;
+
+        if (rs.getString("gerente_login") != null) {
+            Gerente gerente = new Gerente();
+            gerente.setLogin(rs.getString("gerente_login"));
+            gerente.setSenha(rs.getString("gerente_senha"));
+            funcionario = gerente;
+        } else {
+            funcionario = new Funcionario();
+        }
+
+        funcionario.setId(rs.getLong("id_funcionario"));
+        funcionario.setNome(rs.getString("nm_funcionario"));
+        funcionario.setCpf(rs.getString("nr_cpf"));
+        funcionario.setDataNascimento(rs.getDate("dt_nascimento"));
+
+        if (rs.getString("chefe_login") != null) {
+            Gerente chefe = new Gerente();
+            chefe.setLogin(rs.getString("chefe_login"));
+            chefe.setSenha(rs.getString("chefe_senha"));
+            chefe.setId(rs.getLong("FUNCIONARIO_id_gerente"));
+            funcionario.setGerente(chefe);
+        }
+
+        return funcionario;
+    }
 }
