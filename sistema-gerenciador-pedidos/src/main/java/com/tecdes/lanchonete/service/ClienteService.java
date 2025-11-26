9e19ce53f54b562ff122bb17f849055212ede90f
vinicius-andreazza <viniciuslandreazza@gmail.com>
2025-11-25 21:40:40 -0300

    add: adicionar ClienteService

---
+++ sistema-gerenciador-pedidos/src/main/java/com/tecdes/lanchonete/service/ClienteService.java
@@ -0,0 +1,48 @@
+package com.tecdes.lanchonete.service;
+
+import java.util.List;
+
+import com.tecdes.lanchonete.generalinterfaces.crud.Crud;
+import com.tecdes.lanchonete.model.entity.Cliente;
+import com.tecdes.lanchonete.repository.implementation.IClienteRepository;
+
+public class ClienteService implements Crud<Cliente> {
+
+    private final IClienteRepository iClienteRepository;
+
+    public ClienteService(IClienteRepository iClienteRepository) {
+        this.iClienteRepository = iClienteRepository;
+    }
+
+    @Override
+    public Cliente create(Cliente t) {
+        if(t.getCpf()==null){
+            throw new IllegalArgumentException("Cpf não pode ser nulo");
+        }
+        return iClienteRepository.create(t);
+    }
+
+    @Override
+    public Cliente getById(Long id) {
+        return iClienteRepository.getById(id);
+    }
+
+    @Override
+    public List<Cliente> getAll() {
+        return iClienteRepository.getAll();
+    }
+
+    @Override
+    public void update(Cliente t) {
+        if(t.getId()==null){
+            throw new IllegalArgumentException("Cliente não identificado");
+        }
+        iClienteRepository.update(t);
+    }
+
+    @Override
+    public void delete(Long id) {
+        iClienteRepository.delete(id);
+    }
+    
+}
